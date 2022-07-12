b89355818350ed95f7884682cf3807108cac42aa
JacobLinCool <jacoblincool@gmail.com>
2022-07-12 21:49:50 +0800

    feat: add about

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,6 +4,26 @@
     <div class="flex h-full w-full justify-center p-4">
         <div class="flex w-4/5 max-w-3xl flex-col items-center">
             <h1 class="my-4 text-3xl">關於我們 About</h1>
+            <div>
+                <div class="mb-4">
+                    <h2 class="text-xl">緣起</h2>
+                    <p>
+                        我們認為電腦對局是一個相當有趣的領域，但現今的電腦對局比賽常侷限於特定的棋類遊戲，想要比賽還必須先學會接
+                        API。 <br />
+                        在 NCAIC 中，每年的主題皆不相同，可能是棋類，也有可能是桌遊。 <br />
+                        我們也將參賽對象擴大至有興趣的人類都可以參賽，我們希望不只有大學生參賽，高中生與社會人士也可以一同體驗其中的樂趣。
+                        <br />
+                        此外，我們致力打造對初學者友善的環境，不管是初入電腦對局的新手，或是有豐富經驗的專家，都能在這場競賽中獲益良多！
+                        <br />
+                        我們期許在未來 NCAIC 競賽前，可以舉辦工作坊，邀請專家及參賽者一同交流。
+                    </p>
+                </div>
+
+                <div class="mb-4">
+                    <h2 class="text-xl">歷屆主題</h2>
+                    <p>2022年 第 0 屆 五子棋</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
