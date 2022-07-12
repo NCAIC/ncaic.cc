2f589bf0c2af653fbfa1375970c748e143db0d9e
JacobLinCool <jacoblincool@gmail.com>
2022-07-12 18:09:22 +0800

    feat: add title of stream & about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div></div>
+    <div class="flex h-full w-full justify-center p-4">
+        <div class="flex w-4/5 max-w-3xl flex-col items-center">
+            <h1 class="my-4 text-3xl">關於我們 About</h1>
+        </div>
+    </div>
 </template>
--- src/views/Stream.vue
+++ src/views/Stream.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div></div>
+    <div class="flex h-full w-full justify-center p-4">
+        <div class="flex w-4/5 max-w-3xl flex-col items-center">
+            <h1 class="my-4 text-3xl">競賽直播 Stream</h1>
+        </div>
+    </div>
 </template>
