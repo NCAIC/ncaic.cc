c6282fc7bdc67a0563812c895d9f0f57ea83d203
JacobLinCool <jacoblincool@gmail.com>
2022-07-14 16:10:05 +0800

    feat: add schedule rule

--- src/public.ts
+++ src/public.ts
@@ -1,19 +1,51 @@
 import { reactive } from "vue";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, Timestamp } from "firebase/firestore";
 import { db } from "./user";
 
-export const schedule = reactive<any[]>([]);
+export const schedule = reactive<
+    {
+        id: string;
+        rule: {
+            matches: {
+                type: string;
+                count?: number;
+            }[];
+            rule: string;
+            timeout: {
+                step: number;
+                set: number;
+            };
+        };
+        teams: string[];
+        time: Timestamp;
+    }[]
+>([]);
 
 onSnapshot(collection(db, "schedule"), (snapshot) => {
     schedule.splice(
         0,
         schedule.length,
-        ...snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })),
+        ...snapshot.docs.map((doc) => ({ ...(doc.data() as any), id: doc.id })),
     );
 });
 
-export const teams = reactive<any[]>([]);
+export const teams = reactive<
+    {
+        id: string;
+        name: string;
+        org: string;
+        members: {
+            name: string;
+            email?: string;
+            github?: string;
+        }[];
+    }[]
+>([]);
 
 onSnapshot(collection(db, "team"), (snapshot) => {
-    teams.splice(0, teams.length, ...snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    teams.splice(
+        0,
+        teams.length,
+        ...snapshot.docs.map((doc) => ({ ...(doc.data() as any), id: doc.id })),
+    );
 });
--- src/views/Stream.vue
+++ src/views/Stream.vue
@@ -10,16 +10,34 @@ import { schedule, teams } from "../public";
             <div class="w-full">
                 <div
                     v-for="s in schedule"
-                    class="grid w-full grid-cols-3 rounded-md border border-gray-400 p-2"
+                    class="grid w-full grid-cols-1 rounded-md border border-gray-400 p-2 lg:grid-cols-3"
                 >
-                    <div class="col-span-1 flex flex-col justify-center border-r p-2">
-                        <h2 class="text-xl">{{ s.id }}</h2>
-                        <p>{{ s.time.toDate().toLocaleString() }}</p>
+                    <div
+                        class="col-span-1 flex flex-row justify-around border-b p-2 lg:flex-col lg:justify-center lg:border-b-0 lg:border-r"
+                    >
+                        <div>
+                            <h2 class="text-xl">{{ s.id }}</h2>
+                            <p>{{ s.time.toDate().toLocaleString() }}</p>
+                        </div>
+                        <div>
+                            <p class="text-sm">{{ s.rule.matches.length }} 局 {{ s.rule.rule }}</p>
+                            <p class="text-sm text-gray-500">
+                                {{
+                                    s.rule.matches
+                                        .map((m) => m.type + (m.count ? "-" + m.count : ""))
+                                        .join(", ")
+                                }}
+                            </p>
+                        </div>
                     </div>
-                    <div class="col-span-2 flex items-center justify-center p-2">
-                        <p class="text-lg">{{ teams.find((t) => t.id === s.teams[0]).name }}</p>
-                        <p class="mx-2">v.s.</p>
-                        <p class="text-lg">{{ teams.find((t) => t.id === s.teams[1]).name }}</p>
+                    <div class="flex items-center justify-center p-2 lg:col-span-2">
+                        <p class="text-lg">
+                            {{ teams.find((t) => t.id === s.teams[0])?.name || "無隊伍資訊" }}
+                        </p>
+                        <p class="px-2">v.s.</p>
+                        <p class="text-lg">
+                            {{ teams.find((t) => t.id === s.teams[1])?.name || "無隊伍資訊" }}
+                        </p>
                     </div>
                 </div>
             </div>
