d4796e7f3b2be70493f7d5d8a32bb2b4d0d1dc8b
JacobLinCool <jacoblincool@gmail.com>
2022-07-14 17:41:26 +0800

    feat: add news

--- src/public.ts
+++ src/public.ts
@@ -1,5 +1,5 @@
 import { reactive } from "vue";
-import { collection, query, orderBy, onSnapshot, Timestamp } from "firebase/firestore";
+import { collection, query, orderBy, onSnapshot, Timestamp, where } from "firebase/firestore";
 import { db } from "./user";
 
 export const schedule = reactive<
@@ -49,3 +49,26 @@ onSnapshot(collection(db, "team"), (snapshot) => {
         ...snapshot.docs.map((doc) => ({ ...(doc.data() as any), id: doc.id })),
     );
 });
+
+export const news = reactive<
+    {
+        title: string;
+        content: string;
+        time: Timestamp;
+        pinned?: boolean;
+    }[]
+>([]);
+
+onSnapshot(
+    query(collection(db, "news"), where("time", "<=", new Date()), orderBy("time", "desc")),
+    (snapshot) => {
+        news.splice(
+            0,
+            news.length,
+            ...snapshot.docs.map((doc) => ({ ...(doc.data() as any), id: doc.id })),
+        );
+    },
+);
+
+// @ts-ignore
+window.news = news;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import { reactive, watch } from "vue";
+import { computed, reactive, watch } from "vue";
 import Logo from "@c/Logo.vue";
+import Fade from "@c/Fade.vue";
 import { user } from "../user";
+import { news } from "../public";
+
+const pinned = computed(() => news.filter((n) => n.pinned).slice(0, 3));
 
 const links = reactive([
     user.value
@@ -26,17 +30,54 @@ watch(user, () => {
 </script>
 
 <template>
-    <div class="h-full w-full p-4">
-        <Logo class="origin-top-left lg:scale-150" />
+    <div class="h-full w-full overflow-y-auto">
+        <div class="h-full w-full overflow-hidden p-4">
+            <Logo class="origin-top-left lg:scale-150" />
 
-        <div class="absolute bottom-0 right-0 grid grid-cols-2 gap-2 p-4 md:gap-3 lg:gap-4">
-            <router-link v-for="(link, i) in links" :key="i" :to="link.to" class="rounded-full">
-                <div
-                    class="flex h-36 w-36 flex-col items-center justify-center rounded-full border border-black text-xl transition-all duration-300 hover:h-40 hover:w-40 hover:bg-black hover:text-white md:hover:h-44 md:hover:w-44 lg:text-2xl"
-                >
-                    <span>{{ link.zh }}</span> <span>{{ link.en }}</span>
+            <Fade>
+                <div class="my-2 text-lg lg:translate-y-12" v-if="pinned.length">
+                    <a v-for="news in pinned" :key="news.title" class="mb-2 block" href="#news">
+                        <span class="inline-block border-l-4 border-l-gray-400 px-2">{{
+                            news.time.toDate().toLocaleDateString()
+                        }}</span>
+                        <span class="px-2">{{ news.title }}</span>
+                        <span
+                            class="text-gray-500"
+                            v-html="
+                                news.content.slice(0, 20) + (news.content.length > 20 ? '...' : '')
+                            "
+                        ></span>
+                    </a>
                 </div>
-            </router-link>
+            </Fade>
+
+            <div class="absolute bottom-0 right-0 grid grid-cols-2 gap-2 p-4 md:gap-3 lg:gap-4">
+                <router-link v-for="(link, i) in links" :key="i" :to="link.to" class="rounded-full">
+                    <div
+                        class="flex h-36 w-36 flex-col items-center justify-center rounded-full border border-black text-xl transition-all duration-300 hover:h-40 hover:w-40 hover:bg-black hover:text-white md:hover:h-44 md:hover:w-44 lg:text-2xl"
+                    >
+                        <span>{{ link.zh }}</span> <span>{{ link.en }}</span>
+                    </div>
+                </router-link>
+            </div>
+        </div>
+        <div class="h-full w-full overflow-hidden p-4">
+            <h2 id="news" class="text-3xl">公告</h2>
+            <Fade>
+                <div class="my-2 text-lg" v-if="news.length">
+                    <div
+                        v-for="n in news"
+                        :key="n.title"
+                        class="mb-2 border-l-4 border-l-gray-400 p-2"
+                    >
+                        <span class="inline-block pr-2">
+                            {{ n.time.toDate().toLocaleString() }}
+                        </span>
+                        <span class="px-2">{{ n.title }}</span>
+                        <div class="overflow-auto text-gray-700" v-html="n.content"></div>
+                    </div>
+                </div>
+            </Fade>
         </div>
     </div>
 </template>
