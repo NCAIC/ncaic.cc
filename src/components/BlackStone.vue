6320a789d0b498ff30a7b89649c2949ebd330d92
JacobLinCool <jacoblincool@gmail.com>
2022-07-23 13:19:41 +0000

    fix: keep ratio of stones on small screen

--- src/components/BlackStone.vue
+++ src/components/BlackStone.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="h-16 w-16 rounded-full bg-black text-white ring-1 ring-black lg:h-24 lg:w-24">
+    <div
+        class="aspect-square h-16 w-16 rounded-full bg-black text-white ring-1 ring-black lg:h-24 lg:w-24"
+    >
         <slot />
     </div>
 </template>
--- src/components/WhiteStone.vue
+++ src/components/WhiteStone.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-16 w-16 rounded-full bg-white ring-1 ring-black lg:h-24 lg:w-24">
+    <div class="aspect-square h-16 w-16 rounded-full bg-white ring-1 ring-black lg:h-24 lg:w-24">
         <slot />
     </div>
 </template>
