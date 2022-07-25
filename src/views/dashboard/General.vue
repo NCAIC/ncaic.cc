2d15f8ca5d05cd5cb1909c76a442a55cabe5ca54
JacobLinCool <jacoblincool@gmail.com>
2022-07-25 16:12:00 +0800

    feat: resource links in dashboard

--- src/views/dashboard/General.vue
+++ src/views/dashboard/General.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
 import { user, sign_out } from "../../composables/core";
+
+const resources = {
+    "加入 Discord 伺服器": "https://discord.gg/8EJCYjT8Aa",
+    "NCAIC 報名與使用手冊": "https://hackmd.io/@NCAIC/guide",
+    "GitHub Repository 模板": "https://github.com/NCAIC/NCAIC-2022-Template/generate",
+};
 </script>
 
 <template>
     <div>
         <p class="text-lg">
-            {{ user?.providerData[0].displayName || "無名氏" }} ({{
-                user?.providerData[0].email || "無電子郵件"
-            }})
+            嗨，{{ user?.providerData[0].displayName || "無名氏" }}
+            <span class="text-slate-500">({{ user?.providerData[0].email || "無電子郵件" }}) </span>
         </p>
+
+        <div class="my-2 rounded border p-2">
+            <h2 class="mb-2 text-xl">競賽資源</h2>
+            <ul>
+                <li v-for="(url, title) in resources" class="mb-2">
+                    <a
+                        class="ml-4 text-blue-500 transition-all after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:w-0 after:rounded-sm after:bg-indigo-400 after:transition-all after:content-[''] hover:text-indigo-500 hover:after:w-full"
+                        :href="url"
+                        target="_blank"
+                    >
+                        {{ title }}
+                    </a>
+                </li>
+            </ul>
+        </div>
 
         <button
             class="my-2 rounded border border-gray-600 p-2 transition-all hover:border-rose-600 hover:bg-rose-500 hover:text-white"
